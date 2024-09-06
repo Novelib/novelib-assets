59bb38a956532ed18cc5d8a126115ccf2c5bad89
FHYunCai <fhyuncai@163.com>
2024-09-06 22:33:08 +0800

    Create novelib-main.js

---
+++ novelib-main.js
@@ -0,0 +1,5 @@
+document.getElementById('ifm').onload = function() {
+    let iframeDom = document.getElementsByClassName('hope-c-PJLV-ihpgvVt-css')[0].contentWindow.document;
+    iframeDom.getElementById('viewer').style.top = 0;
+    iframeDom.body.style.minHeight = "unset";
+}
